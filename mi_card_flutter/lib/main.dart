41de8aba1a1d649dd358ae321bc5e740f9472e35
Pritish Khera <pritish.khera@gmail.com>
2023-08-21 23:35:46 +0530

    commit

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -5,41 +5,80 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return  MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-            child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Container(
-              width: 100,
-              height: double.infinity,
-              color: Colors.red,
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.yellow,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('images/pritish.jpg'),
+              ),
+              Text(
+                'Pritish Khera',
+                style: TextStyle(
+                    fontSize: 40.0,
+                    fontFamily: 'Pacifico',
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text('FLUTTER DEVELOPER',
+                  style: TextStyle(
+                      fontSize: 20.0,
+                      fontFamily: 'SourceSans3',
+                      color: Colors.tealAccent,
+                      letterSpacing: 2.5,
+                      fontWeight: FontWeight.w900)),
+              SizedBox(height: 20,
+                width: 150,
+                child: Divider(
+                  color: Colors.tealAccent,
                 ),
-                Container(height: 100, width: 100, color: Colors.green)
-              ],
-            ),
-            Container(
-              width: 100,
-              height: double.infinity,
-              color: Colors.blue,
-            )
-          ],
-        )),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+91 9811179076',
+                    style: TextStyle(
+                        color: Colors.teal,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'SourceSans3'),
+                  ),
+
+                )
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(Icons.email,
+                    color: Colors.teal,),
+                  title: Text('pritish.khera@gmail.com',
+                    style: TextStyle(
+                        color: Colors.teal,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'SourceSans3'),
+                  ),
+                )
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
 }
+
+
