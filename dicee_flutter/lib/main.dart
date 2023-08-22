90c029fcde71f91a0d383ebf6ef1cff85b766787
Pritish Khera <pritish.khera@gmail.com>
2023-08-22 16:20:09 +0530

    using android studio

---
+++ dicee_flutter/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Center(child: Text('Dicee')),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Image.asset('images/dice1.png')
+          ),
+        
+        Expanded(
+          child: Image.asset('images/dice2.png')
+        )
+      ],
+    );
+  }
+}
