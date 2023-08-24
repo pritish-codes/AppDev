8346d4b5f4da7ebff4650dd3a4196503ccd297a5
Pritish Khera <pritish.khera@gmail.com>
2023-08-24 11:44:23 +0530

    using android studio

--- dicee_flutter/lib/main.dart
+++ dicee_flutter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,19 +16,49 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(5) + 1;
+      rightDiceNumber = Random().nextInt(5) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(
-          child: Image.asset('images/dice1.png')
-          ),
-        
-        Expanded(
-          child: Image.asset('images/dice2.png')
-        )
-      ],
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+              child: TextButton(
+                  onPressed: () {
+                    changeDiceFace();
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.asset('images/dice$leftDiceNumber.png'),
+                  ))),
+          Expanded(
+              child: TextButton(
+                  onPressed: () {
+                    changeDiceFace();
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.asset('images/dice$rightDiceNumber.png'),
+                  )))
+        ],
+      ),
     );
   }
 }
